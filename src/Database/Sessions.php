4d479742057e987cd2a4f128a48e6d387cd59508
Pierre Christensen <pierre.christensen@gmail.com>
2021-05-09 10:16:51 +0200

    handle case with table missing error

--- src/Database/Sessions.php
+++ src/Database/Sessions.php
@@ -2,13 +2,31 @@
 
 namespace Pitch7900\Database;
 
+use Exception;
+use ErrorException;
+
 /**
  * Class Track for Illuminate (DB) queries
  * Need to create a database with illuminate
  */
-class Sessions extends AbstractModel {
+class Sessions extends AbstractModel
+{
     public $timestamps = true;
     protected $table = 'sessions';
     protected $primaryKey = 'id';
-    protected $fillable = ['id','data','timestamp'];  
+    protected $fillable = ['id', 'data', 'timestamp'];
+
+    public static function exists()
+    {
+        if (!Sessions::hasTable(Sessions::$table)) {
+            $sqlscript = __DIR__."/../../documents/scripts.sql";
+            if (file_exists($sqlscript)){
+                $file = fopen($sqlscript, "r");
+                fclose($sqlscript);
+                throw new Exception("Table :".Sessions::$table." is missing !\nPlease create using the following script: \n".$file);
+            } else {
+                throw new Exception("Table :".Sessions::$table." is missing !\n");
+            }
+        }
+    }
 }
